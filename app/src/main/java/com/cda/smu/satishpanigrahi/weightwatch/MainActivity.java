036959a31cb46cb55d4d61aa9cd9961b9fce7a65
satishPanigrahi <satish.panigrahi@gmail.com>
2016-03-31 18:34:07 -0300

    Added: 1) LoginDataBaseAdapter and 2) DataBaseHelper

--- app/src/main/java/com/cda/smu/satishpanigrahi/weightwatch/MainActivity.java
+++ app/src/main/java/com/cda/smu/satishpanigrahi/weightwatch/MainActivity.java
@@ -2,8 +2,14 @@ package com.cda.smu.satishpanigrahi.weightwatch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    // button
+    Button btnSignIn,btnSignUp;
+   // LoginDataBaseAdapter loginDataBaseAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
