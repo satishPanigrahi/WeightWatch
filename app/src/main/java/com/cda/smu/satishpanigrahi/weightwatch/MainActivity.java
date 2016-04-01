5c51176548e1041f46b42cfc27bdec3335d558cb
satishPanigrahi <satish.panigrahi@gmail.com>
2016-03-31 22:05:28 -0300

    Corrected naming error in login Activity and Signup activity

--- app/src/main/java/com/cda/smu/satishpanigrahi/weightwatch/MainActivity.java
+++ app/src/main/java/com/cda/smu/satishpanigrahi/weightwatch/MainActivity.java
@@ -1,19 +1,52 @@
 package com.cda.smu.satishpanigrahi.weightwatch;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    // button
+
     Button btnSignIn,btnSignUp;
-   // LoginDataBaseAdapter loginDataBaseAdapter;
+    LoginDataBaseAdapter loginDataBaseAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //create instance of SQlite Database
+        loginDataBaseAdapter = new LoginDataBaseAdapter(this);
+        loginDataBaseAdapter = loginDataBaseAdapter.open();
+
+        // Get The Reference Of Buttons
+        btnSignIn=(Button)findViewById(R.id.buttonSignIN);
+        btnSignUp=(Button)findViewById(R.id.buttonSignUP);
+
+        // Set onClick listener on SignUP button
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Create indent for SignupActivity and start the activity
+               // Intent intentSignUp = new Intent(getApplicationContext().activity_signup.class);
+
+
+            }
+        });
+
+
+
+
     }
+
+
+
+
+
 }
